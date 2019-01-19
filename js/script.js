b10ab0cd0d8163506833d10b7e9ea44f9458f732
Ernest Ojeh <ernestojeh@gmail.com>
2019-01-20 00:16:30 +0100

    Added animate css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-$(".single-project").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
-     $(this).removeClass("move-up");
+import animate from "https://cdn.jsdelivr.net/npm/animateplus@2/animateplus.js";
+
+animate({
+  elements: ".animate-wrap",
+  easing: "in-out-circular",
+  transform: ["translateY(60px)", "translateY(0px)"],
+  duration: 800,
+  opacity: [0, 1]
 });
