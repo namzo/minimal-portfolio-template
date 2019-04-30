9feea7b673b9116aa7fe800051d3ddcede8c73a6
Patrick Ojeh <patrickojeh@gmail.com>
2019-04-30 23:38:42 +0100

    modified js to include code for fetching exif data from image

--- _site/js/script.js
+++ _site/js/script.js
@@ -7,3 +7,26 @@ animate({
   duration: 800,
   opacity: [0, 1]
 });
+
+function getExif() {
+  var image = document.querySelector(".single-photo_wrap img");
+  EXIF.getData(image, function() {
+    let focalLengthVal = EXIF.getTag(this, "FocalLength");
+    let apertureVal = EXIF.getTag(this, "ApertureValue");
+    const shutterSpeedVal = EXIF.getTag(this, "ShutterSpeedValue");
+    const ISOSpeedVal = EXIF.getTag(this, "ISOSpeedRatings");
+
+    focalLengthVal = focalLengthVal['numerator'] / focalLengthVal['denominator'];
+    apertureVal = apertureVal['numerator'] / apertureVal['denominator'];
+
+    const focal = document.querySelector('p.focal-length');
+    const aperture = document.querySelector('p.aperture');
+    const shutter = document.querySelector('p.shutter-speed');
+    const iso = document.querySelector('p.iso');
+
+    focal.innerHTML = `${focalLengthVal}mm`;
+    aperture.innerHTML = `ƒ/${apertureVal}`;
+    shutter.innerHTML = `1/${shutterSpeedVal}s`;
+    iso.innerHTML = `${ISOSpeedVal}`;
+  });
+}
